a27ff2d8b7c578b95deb7c98d1db597acd9cf50d
Marcos Frankowicz <marcosfrankowicz@gmail.com>
2022-12-29 10:47:47 -0300

    requests test

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    url = "https://api.intelipost.com.br/api/v1/shipment_order/invoice/66511"
+
+    payload = ""
+    headers = {
+        "Content-Type": "application/json",
+        "api-key": "312a8fb1734ce968c4e2e3cf2c4a9ef6bd1efdac7d6fcde513acfdb99a4c2727"
+    }
+
+    response = requests.request("GET", url, data=payload, headers=headers)
+
+    return response.text
